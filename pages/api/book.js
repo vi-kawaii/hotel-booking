9fdedee3bdda6bd33533603788b3ebe80a077280
vi-wivi <velianjanjan@gmail.com>
2023-07-04 23:13:43 +0500

    пересекающееся время не добавить

--- pages/api/book.js
+++ pages/api/book.js
@@ -1,13 +1,34 @@
 import prisma from "../../lib/prisma";
+import areIntervalsOverlapping from "date-fns/areIntervalsOverlapping";
 
 export default async function handler(req, res) {
-  const { roomId, from, to, is_vip } = req.body;
+  const { room_id, from, to, is_vip } = JSON.parse(req.body);
+
+  const bookings = await prisma.booking.findMany();
+
+  let isNumberBusy = false;
+
+  bookings.some((b) => {
+    if (
+      areIntervalsOverlapping(
+        { start: from, end: to },
+        { start: b.from, end: b.from }
+      )
+    ) {
+      isNumberBusy = true;
+    }
+  });
+
+  if (isNumberBusy) {
+    res.end();
+    return;
+  }
 
   await prisma.booking.create({
     data: {
-      roomId,
-      from,
-      to,
+      room_id,
+      from: new Date(from),
+      to: new Date(to),
       is_vip,
     },
   });
