f3df1c921982410b7f7f69e34cbe7e72f9c0cc10
vi-wivi <velianjanjan@gmail.com>
2023-07-04 23:19:59 +0500

    можно посмотреть свободные комнаты

--- pages/api/rooms/free.js
+++ pages/api/rooms/free.js
@@ -1,7 +1,12 @@
 import prisma from "../../../lib/prisma";
 
 export default async function handler(req, res) {
-  const result = await prisma.room.findMany();
+  const bookings = await prisma.booking.findMany();
+  const rooms = await prisma.room.findMany();
+
+  const bookingsRooms = bookings.map((b) => b.room_id);
+
+  const result = rooms.filter((r) => !bookingsRooms.includes(r.id));
 
   res.json(result);
 }
