35dc16f114874599d806ced576e038c4fbb1c765
vi-wivi <velianjanjan@gmail.com>
2023-07-04 23:15:18 +0500

    работает удаление брони

--- pages/api/cancel.js
+++ pages/api/cancel.js
@@ -1,7 +1,7 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-  const { id } = req.body;
+  const { id } = JSON.parse(req.body);
 
   await prisma.booking.delete({
     where: {
